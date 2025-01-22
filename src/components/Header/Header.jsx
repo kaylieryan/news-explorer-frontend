61fdb928577f1ba298d6d97193f7789927f6ff01
kaylieryan <kayliemryan@gmail.com>
2025-01-22 15:04:44 +0000

    fix ' error.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 
 import { Link } from "react-router-dom";
 import "./Header.css";
+// import SearchForm from "../SearchForm/SearchForm";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__nav-container">
         <div className="header__text-container">
-          <h1 className="header__title">What's going on in the world?</h1>
+          <h1 className="header__title">What&apos;s going on in the world?</h1>
           <h2 className="header__subtitle">
             Find the latest news on any topic and save them in your personal
             account
