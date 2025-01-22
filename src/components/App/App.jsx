f06034053f4e307e772e7efcbc31e1b428e653fe
kaylieryan <kayliemryan@gmail.com>
2025-01-22 11:52:32 +0000

    Build Footer.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,21 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "../Main/Main.jsx";
 import About from "../About/About.jsx";
 import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import "./Footer.css";
+import facebook from "../../assets/images/facebook.svg";
+import github from "../../assets/images/github.svg";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">© 2024 Supersite, Powered by News API</p>
+      <div className="footer__links-container">
+        <Link to="/" className="footer__link footer__link-home">
+          <p className="footer__home">Home</p>
+        </Link>
+        <a
+          href="https://tripleten.com/"
+          target="_blank"
+          className="footer__link footer__link-tripleten">
+          TripleTen
+        </a>
+        <a
+          href="https://github.com/Andrew-W01"
+          target="_blank"
+          className="footer__link footer__link-github">
+          <img src={github} alt="" className="footer__icon" />
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          className="footer__link footer__link-linkedin">
+          <img src={facebook} alt="" className="footer__icon" />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
