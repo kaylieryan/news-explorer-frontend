2b018c6a8731f2dce0412dfb97ce48b098ebc868
kaylieryan <kayliemryan@gmail.com>
2025-01-28 14:38:51 +0000

    create and style header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,19 @@
-
-import { Link } from "react-router-dom";
 import "./Header.css";
-// import SearchForm from "../SearchForm/SearchForm";
+import Navigation from "../Navigation/Navigation";
+import SearchForm from "../SearchForm/SearchForm";
 
-function Header() {
+function Header({ onLoginClick, onLogout, onRegisterClick, handleSearch }) {
   return (
-    <header className="header">
-      <div className="header__nav-container">
-        <div className="header__text-container">
-          <h1 className="header__title">What&apos;s going on in the world?</h1>
-          <h2 className="header__subtitle">
-            Find the latest news on any topic and save them in your personal
-            account
-          </h2>
-        </div>
-        <div className="header__logo">NewsExplorer</div>
-        <nav className="header__nav">
-          <Link to="/" className="header__link">
-            Home
-          </Link>
-          <button className="header__button">Sign In</button>
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <Navigation
+          onLoginClick={onLoginClick}
+          onLogout={onLogout}
+          onRegisterClick={onRegisterClick}
+        />
+        <SearchForm handleSearch={handleSearch} />
+      </header>
+    </>
   );
 }
 
