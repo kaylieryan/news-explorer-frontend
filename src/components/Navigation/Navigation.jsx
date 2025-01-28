c131536f135fbcc36326a91195d8b3cfb03c833c
kaylieryan <kayliemryan@gmail.com>
2025-01-28 14:45:37 +0000

    reconfig nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,44 +1,130 @@
-import "../Navigation/Navigation.css";
-import NavLoggedIn from "../NavLoggedIn/NavLoggedIn";
-import NavLoggedOut from "../NavLoggedOut/NavLoggedOut";
-import Main from "../Main/Main";
-import { useState } from "react";
+import "./Navigation.css";
+import headerWhiteLogo from "../../assets/headerWhiteLogo.svg";
+import headerBlackLogo from "../../assets/headerBlackLogo.svg";
+import logOutBlack from "../../assets/logout_black.svg";
+import logOutWhite from "../../assets/logout_white.svg";
+import { NavLink } from "react-router-dom";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import { useContext } from "react";
+import { currentPageContext } from "../../contexts/currentPageContext";
+import { mobileContext } from "../../contexts/mobileContext";
+import { currentUserContext } from "../../contexts/currentUserContext";
 
-function Nav({ handleLoginClick, isInverse, isLoggedIn, handleLogout }) {
-  const [isClicked, setIsClicked] = useState(false);
+function Navigation({ onLoginClick, onLogout }) {
+  const { currentPage, activeModal } = useContext(currentPageContext);
+  const { currentUser, isLoggedIn } = useContext(currentUserContext);
+  const { mobileMenuOpen, openMobileMenu, closeMobileMenu } =
+    useContext(mobileContext);
 
-  const handleMainClick = () => {
-    setIsClicked(true);
-  };
-
-  const handleMainCloseClick = () => {
-    setIsClicked(false);
+  const handleMobileMenu = () => {
+    if (mobileMenuOpen) {
+      closeMobileMenu();
+    } else {
+      openMobileMenu();
+    }
   };
 
   return (
-    <nav className="nav" data-theme={isInverse ? "light" : "dark"}>
-      <p className="nav__logo">NewsExplorer</p>
-      <button
-        onClick={handleMainClick}
-        className={
-          isInverse ? "nav__menu-btn nav__menu-btn--black" : "nav__menu-btn"
-        }></button>
-
-      {!isLoggedIn ? (
-        <NavLoggedOut handleLoginClick={handleLoginClick} />
-      ) : (
-        <NavLoggedIn isInverse={isInverse} handleLogout={handleLogout} />
-      )}
-      {isClicked ? (
-        <Main
-          isLoggedIn={isLoggedIn}
-          onClose={handleMainCloseClick}
-          handleLoginClick={handleLoginClick}
-          handleLogout={handleLogout}
+    <nav
+      className={`nav ${
+        currentPage === "/saved-news" ? "nav__saved-news-nav" : ""
+      } ${mobileMenuOpen ? "nav__menu-open" : ""}`}>
+      <nav>
+        {currentPage === "/" || mobileMenuOpen ? (
+          <NavLink to="/">
+            <img
+              src={headerWhiteLogo}
+              alt="NewsExplorer White Logo"
+              className="nav__logo-white"
+            />
+          </NavLink>
+        ) : (
+          <NavLink to="/">
+            <img
+              src={headerBlackLogo}
+              alt="NewsExplorer Black Logo"
+              className="nav__logo-black"
+            />
+          </NavLink>
+        )}
+      </nav>
+      {currentPage === "/" ? (
+        <button
+          className={`nav__menu-button ${
+            activeModal === "" ? "nav__menu-button_hidden" : ""
+          } ${mobileMenuOpen === true ? "nav__menu-button_close" : ""}`}
+          onClick={handleMobileMenu}
         />
-      ) : null}
+      ) : (
+        <button
+          className={`nav__saved-news-menu-button ${
+            activeModal === "" ? "nav__saved-news-menu-button_hidden" : ""
+          } ${
+            mobileMenuOpen === true ? "nav__saved-news-menu-button_close" : ""
+          }`}
+          onClick={handleMobileMenu}
+        />
+      )}
+
+      {mobileMenuOpen && (
+        <MobileMenu onLoginClick={onLoginClick} onLogout={onLogout} />
+      )}
+
+      {isLoggedIn && currentPage === "/" ? (
+        <nav className="nav__user-container">
+          <NavLink to="/" className="nav__button-home">
+            Home
+          </NavLink>
+          <NavLink to="/saved-news" className="nav__button-saved-articles">
+            Saved Articles
+          </NavLink>
+          <button
+            className={`nav__button-loggedin ${
+              currentPage === "/" ? "nav__button-loggedin-white" : ""
+            }`}
+            onClick={onLogout}>
+            <span className="nav__username">{currentUser.name}</span>
+            <img
+              src={currentPage === "/" ? logOutWhite : logOutBlack}
+              alt="logout"
+              className="nav__logout-icon"
+            />
+          </button>
+        </nav>
+      ) : isLoggedIn && currentPage === "/saved-news" ? (
+        <nav className="nav__user-container">
+          <NavLink to="/" className="nav__saved__news-button-home">
+            Home
+          </NavLink>
+          <NavLink to="/saved-news" className="nav__button-saved-articles-user">
+            Saved Articles
+          </NavLink>
+          <button
+            className={`nav__button-loggedin-black ${
+              currentPage === "/" ? "nav__button-loggedin" : ""
+            }`}
+            onClick={onLogout}>
+            <span className="nav__username">{currentUser.name}</span>
+            <img
+              src={currentPage === "/" ? logOutWhite : logOutBlack}
+              alt="logout"
+              className="nav__logout-icon"
+            />
+          </button>
+        </nav>
+      ) : (
+        <div
+          className={`nav__buttons ${mobileMenuOpen ? "nav__menu-open" : ""}`}>
+          <NavLink to="/" className="nav__button-home">
+            Home
+          </NavLink>
+          <button className="nav__button-signin" onClick={onLoginClick}>
+            Sign in
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
 
-export default Nav;
+export default Navigation;
