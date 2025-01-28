e5027effdb06d37dd994ceec2db6ebb30755b324
kaylieryan <kayliemryan@gmail.com>
2025-01-28 14:42:46 +0000

    reconfig main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,63 @@
 import "./Main.css";
+import About from "../About/About";
+import Header from "../Header/Header";
+import NewsCardList from "../NewsCardList/NewsCardList";
+import Preloader from "../Preloader/Preloader";
+import NotFound from "../NotFound/NotFound";
+import { useContext } from "react";
+import { searchResultContext } from "../../contexts/searchResultContext";
+import { hasSearchedContext } from "../../contexts/hasSearchedContext";
+import { currentUserContext } from "../../contexts/currentUserContext";
 
-function Main() {
+function Main({
+  searchError,
+  isLoading,
+  handleSearch,
+  handleRemoveArticle,
+  handleSaveArticle,
+  onLoginClick,
+  onLogout,
+  onRegisterClick,
+}) {
+  const { searchResult } = useContext(searchResultContext);
+  const { hasSearched } = useContext(hasSearchedContext);
+  const { isLoggedIn } = useContext(currentUserContext);
+
   return (
-    <div>
-      <h1>Main Page</h1>
-      <p>Welcome to the main page!</p>
-    </div>
+    <>
+      <Header
+        onLoginClick={onLoginClick}
+        onLogout={onLogout}
+        onRegisterClick={onRegisterClick}
+        handleSearch={handleSearch}
+      />
+      <main className="main">
+        {isLoggedIn && (
+        <div>
+          {hasSearched && searchResult.length > 0 ? (
+            <NewsCardList
+              onLoginClick={onLoginClick}
+              handleSaveArticle={handleSaveArticle}
+              handleRemoveArticle={handleRemoveArticle}
+            />
+          ) : hasSearched && searchResult.length === 0 ? (
+            <NotFound />
+          ) : isLoading ? (
+            <Preloader />
+          ) : searchError === true ? (
+            <p>
+              Sorry, something went wrong during the request. There may be a
+              connection issue or the server may be down. Please try again
+              later.
+            </p>
+          ) : (
+            ""
+          )}
+        </div>
+        )}
+        <About />
+      </main>
+    </>
   );
 }
 
