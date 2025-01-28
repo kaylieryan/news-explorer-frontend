0c39dad829863f240a15da7f668ce8fbb5e6fab7
kaylieryan <kayliemryan@gmail.com>
2025-01-28 14:41:36 +0000

    reconfigure search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,35 +1,91 @@
-import "../SearchForm/SearchForm.css";
-import { useState } from "react";
+// import "./SearchForm.css";
+// import { useContext } from "react";
+// import { keywordContext } from "../../contexts/keyWordContext";
 
-function SearchForm({ handleSearchSubmit, setCurrentKeyword }) {
-  const [value, setValue] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleSearchSubmit(e);
+// function SearchForm({ handleSearch }) {
+//   const { keyword, setkeyword } = useContext(keywordContext);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     handleSearch(keyword);
+//   };
+
+//   const handleKeyWord = (event) => {
+//     setkeyword(event.target.value);
+//   };
+
+//   return (
+//     <div className="search-form">
+//       <section className="search-form__container">
+//         <h1 className="search-form__header">What's going on in the world?</h1>
+//         <p className="search-form__subHeader">
+//           Find the latest news on any topic and save them in your personal
+//           account.
+//         </p>
+//         <form className="search-form" onSubmit={handleSubmit}>
+//           <form className="search-form__bar">
+//             <input
+//               className="search-form__input"
+//               type="text"
+//               id="search"
+//               value={keyword}
+//               placeholder="Enter Topic"
+//               onChange={handleKeyWord}
+//               required
+//             />
+//           </form>
+//           <button type="submit" className="search-form__button">
+//             Search
+//           </button>
+//         </form>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default SearchForm;
+
+import "./SearchForm.css";
+import { useContext } from "react";
+import { keywordContext } from "../../contexts/keyWordContext";
+
+function SearchForm({ handleSearch }) {
+  const { keyword, setkeyword } = useContext(keywordContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSearch(keyword);
   };
 
-  const handleChange = (e) => {
-    const newValue = e.target.value;
-    setValue(newValue);
-    setCurrentKeyword(newValue);
+  const handleKeyWord = (event) => {
+    setkeyword(event.target.value);
   };
 
   return (
-    <div className="search">
-      <form action="" onSubmit={handleSubmit} className="search__form">
-        <div className="search__input-container">
-          <input
-            onChange={handleChange}
-            type="text"
-            className="search__input"
-            placeholder="Enter topic"
-            maxLength="20"
-            required
-            value={value || ""}
-          />
-          <button className="search__submit-button">Search</button>
-        </div>
-      </form>
+    <div className="search-form">
+      <section className="search-form__container">
+        <h1 className="search-form__header">What's going on in the world?</h1>
+        <p className="search-form__subHeader">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+        <form className="search-form" onSubmit={handleSubmit}>
+          <div className="search-form__bar">
+            <input
+              className="search-form__input"
+              type="text"
+              id="search"
+              value={keyword}
+              placeholder="Enter Topic"
+              onChange={handleKeyWord}
+              required
+            />
+          </div>
+          <button type="submit" className="search-form__button">
+            Search
+          </button>
+        </form>
+      </section>
     </div>
   );
 }
