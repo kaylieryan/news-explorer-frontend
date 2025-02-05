4b23dfbd742723a0b9177f35b2796f38cba1972c
kaylieryan <kayliemryan@gmail.com>
2025-02-05 11:47:53 +0000

    Links in footer should open in new tabs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,14 +11,16 @@ function Footer() {
       <div className="footer__links-wrapper">
         <ul className="footer__links">
           <li>
-            <a href="#" className="footer__link footer__link--home">
+            <a href="#" className="footer__link footer__link--home" target="_blank" rel="noopener noreferrer">
               Home
             </a>
           </li>
           <li>
             <a
               href="https://tripleten.com/"
-              className="footer__link footer__link--triplenTen">
+              className="footer__link footer__link--triplenTen"
+              target="_blank"
+              rel="noopener noreferrer">
               TriplenTen
             </a>
           </li>
@@ -27,7 +29,9 @@ function Footer() {
           <li>
             <a
               href="https://github.com/kaylieryan"
-              className="footer__link footer__link--github">
+              className="footer__link footer__link--github"
+              target="_blank"
+              rel="noopener noreferrer">
               <img
                 src={githubLogo}
                 alt="GitHub Logo"
@@ -38,7 +42,9 @@ function Footer() {
           <li>
             <a
               href="https://www.facebook.com/"
-              className="footer__link footer__link--facebook">
+              className="footer__link footer__link--facebook"
+              target="_blank"
+              rel="noopener noreferrer">
               <img
                 src={facebookLogo}
                 alt="Facebook Logo"
