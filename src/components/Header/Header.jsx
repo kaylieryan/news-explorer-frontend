0d54db30099e48e97086f364e9b20e4695843a28
kaylieryan <kayliemryan@gmail.com>
2025-02-08 13:44:10 +0000

    split header and search form properly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,14 @@ function Header({ onLoginClick, onLogout, onRegisterClick, handleSearch }) {
           onLogout={onLogout}
           onRegisterClick={onRegisterClick}
         />
-        <SearchForm handleSearch={handleSearch} />
+        <div className="header__content">
+          <h1 className="header__header">What's going on in the world?</h1>
+          <p className="header__subHeader">
+            Find the latest news on any topic and save them in your personal
+            account.
+          </p>
+          <SearchForm handleSearch={handleSearch} />
+        </div>
       </header>
     </>
   );
