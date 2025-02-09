4d6ce7eb452836433e160ba07bb5c6bcb9e01cd5
kaylieryan <kayliemryan@gmail.com>
2025-02-09 10:44:25 +0000

    rename class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header({ onLoginClick, onLogout, onRegisterClick, handleSearch }) {
           onRegisterClick={onRegisterClick}
         />
         <div className="header__content">
-          <h1 className="header__header">What's going on in the world?</h1>
+          <h1 className="header__title">What's going on in the world?</h1>
           <p className="header__subHeader">
             Find the latest news on any topic and save them in your personal
             account.
