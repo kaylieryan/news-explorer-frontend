0c8c0eed47bd7b79928631570594b25e066b40f8
kaylieryan <kayliemryan@gmail.com>
2025-02-09 11:30:50 +0000

    responsiveness to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,19 +6,21 @@ function Header({ onLoginClick, onLogout, onRegisterClick, handleSearch }) {
   return (
     <>
       <header className="header">
-        <Navigation
-          onLoginClick={onLoginClick}
-          onLogout={onLogout}
-          onRegisterClick={onRegisterClick}
-        />
-        <div className="header__content">
+        <div className="header__nav-container">
+          <Navigation
+            onLoginClick={onLoginClick}
+            onLogout={onLogout}
+            onRegisterClick={onRegisterClick}
+          />
+        </div>
+        <div className="header__text-container">
           <h1 className="header__title">What's going on in the world?</h1>
-          <p className="header__subHeader">
+          <h2 className="header__subtitle">
             Find the latest news on any topic and save them in your personal
             account.
-          </p>
-          <SearchForm handleSearch={handleSearch} />
+          </h2>
         </div>
+        <SearchForm handleSearch={handleSearch} />
       </header>
     </>
   );
