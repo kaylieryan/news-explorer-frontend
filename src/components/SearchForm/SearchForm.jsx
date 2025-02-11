09dd64db7d500fdf2cb97e9f8c6936a7c1ebf12a
kaylieryan <kayliemryan@gmail.com>
2025-02-11 14:32:40 +0000

    sizing of search form.

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -18,20 +18,24 @@ function SearchForm({ handleSearch }) {
   };
 
   return (
-    <form className="search-form__bar" onSubmit={handleSubmit}>
-      <input
-        className="search-form__input"
-        type="text"
-        id="search"
-        value={keyword}
-        placeholder="Enter Topic"
-        onChange={handleKeyWord}
-        required
-      />
-      <button type="submit" className="search-form__button">
-        Search
-      </button>
-    </form>
+    <div className="search">
+      <form action="" onSubmit={handleSubmit} className="search__form">
+        <div className="search__input-container">
+        <input
+          className="search__input"
+          type="text"
+          id="search"
+          value={keyword}
+          placeholder="Enter Topic"
+          onChange={handleKeyWord}
+          required
+        />
+        <button type="submit" className="search__submit-button">
+          Search
+        </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
