258664c7f563d9d3ed2a0811b53629b5ad15d64d
kaylieryan <kayliemryan@gmail.com>
2025-02-13 13:35:34 +0000

    About refactor.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,24 @@
 import "./About.css";
 import aboutImage from "../../assets/aboutPicture.png";
+
 function About() {
   return (
     <section className="about">
-      <img src={aboutImage} alt="aboutImage" className="about__image" />
-      <div className="about__content">
-        <h2 className="about__title">About the author</h2>
+      <div className="about__image-container">
+        <img src={aboutImage} alt="aboutImage" className="about__image" />
+      </div>
+      <div className="about__info">
+        <h3 className="about__title">About the author</h3>
         <p className="about__paragraph">
           Hello, I'm Kaylie Ryan. I'm a full stack engineer proficient in
-          JavaScript, React, Express, Node, HTML, CSS. I own a hair salon 
-          and was a hair stylist before becoming a software engineer. 
+          JavaScript, React, Express, Node, HTML, CSS. I own a hair salon and
+          was a hair stylist before becoming a software engineer.
         </p>
         <p className="about__paragraph">
-          I started learning to code at TripleTen Bootcamp. Learning to 
-          code has been a difficult yet rewarding journey. I love the
-          problem solving aspect of coding and the endless possibilities
-          that come with it. I'm excited to continue to learn and grow
-          as a software engineer.
+          I started learning to code at TripleTen Bootcamp. Learning to code has
+          been a difficult yet rewarding journey. I love the problem solving
+          aspect of coding and the endless possibilities that come with it. I'm
+          excited to continue to learn and grow as a software engineer.
         </p>
       </div>
     </section>
