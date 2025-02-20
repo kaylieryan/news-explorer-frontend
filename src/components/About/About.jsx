8720685deca0eb7661830908718f981a8fec5500
kaylieryan <kayliemryan@gmail.com>
2025-02-20 11:43:02 +0000

    shorten about.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,14 +11,11 @@ function About() {
         <h3 className="about__title">About the author</h3>
         <p className="about__paragraph">
           Hello, I'm Kaylie Ryan. I'm a full stack engineer proficient in
-          JavaScript, React, Express, Node, HTML, CSS. I own a hair salon and
-          was a hair stylist before becoming a software engineer.
+          JavaScript, React, Express, Node, HTML, CSS.
         </p>
         <p className="about__paragraph">
           I started learning to code at TripleTen Bootcamp. Learning to code has
-          been a difficult yet rewarding journey. I love the problem solving
-          aspect of coding and the endless possibilities that come with it. I'm
-          excited to continue to learn and grow as a software engineer.
+          been a difficult yet rewarding journey. 
         </p>
       </div>
     </section>
