bb81bc9a7ec8c3745febe0381c4c05f03fa74ec8
kaylieryan <kayliemryan@gmail.com>
2025-02-23 17:55:47 +0000

    BEM specifications.

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,12 +1,13 @@
 import SavedNewsHeader from "../SavedNewsHeader/SavedNewsHeader";
 import SavedNewsCardList from "../SavedNewsCardList/SavedNewsCardList";
 import Navigation from "../Navigation/Navigation";
+import "./SavedNews.css";
 
 const SavedNews = ({ handleRemoveArticle }) => {
   return (
     <>
       <Navigation />
-      <section className="saved">
+      <section className="saved-news">
         <SavedNewsHeader />
         <SavedNewsCardList handleRemoveArticle={handleRemoveArticle} />
       </section>
