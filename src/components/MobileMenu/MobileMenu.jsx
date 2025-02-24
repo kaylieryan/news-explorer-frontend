fd5586d51e9dafc1ff9906423d93d4cb98427ffd
kaylieryan <kayliemryan@gmail.com>
2025-02-24 18:36:34 +0000

    mobile menu not logged in.

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,5 +1,7 @@
 import "./MobileMenu.css";
 import { NavLink } from "react-router-dom";
+import headerWhiteLogo from "../../assets/headerWhiteLogo.svg";
+import headerBlackLogo from "../../assets/headerBlackLogo.svg";
 
 function MobileMenu({
   isOpen,
@@ -15,54 +17,70 @@ function MobileMenu({
   return (
     <div className={`mobile-menu ${isOpen ? "mobile-menu--open" : ""}`}>
       <div className="mobile-menu__content">
-        <NavLink
-          to="/"
-          className={`mobile-menu__link ${
-            currentPage === "/" ? "mobile-menu__link--active" : ""
-          }`}
-          onClick={onClose}>
-          Home
-        </NavLink>
+        <div className="mobile-menu__header">
+          <NavLink to="/" className="mobile-menu__logo-link" onClick={onClose}>
+            <img
+              src={currentPage === "/" ? headerWhiteLogo : headerBlackLogo}
+              alt="NewsExplorer Logo"
+              className="mobile-menu__logo"
+            />
+          </NavLink>
+          <button className="mobile-menu__close-button" onClick={onClose}>
+            <span className="mobile-menu__close-button-line"></span>
+            <span className="mobile-menu__close-button-line"></span>
+          </button>
+        </div>
 
-        {isLoggedIn && (
+        <div className="mobile-menu__nav">
           <NavLink
-            to="/saved-news"
+            to="/"
             className={`mobile-menu__link ${
-              currentPage === "/saved-news" ? "mobile-menu__link--active" : ""
+              currentPage === "/" ? "mobile-menu__link--active" : ""
             }`}
             onClick={onClose}>
-            Saved articles
+            Home
           </NavLink>
-        )}
 
-        {isLoggedIn ? (
-          <button
-            className={`mobile-menu__button mobile-menu__button--logged-in ${
-              currentPage === "/"
-                ? "mobile-menu__button--white"
-                : "mobile-menu__button--black"
-            }`}
-            onClick={() => {
-              onLogout();
-              onClose();
-            }}>
-            <span className="mobile-menu__username">{currentUser.name}</span>
-            <img
-              src={currentPage === "/" ? logOutWhite : logOutBlack}
-              alt="logout"
-              className="mobile-menu__logout-icon"
-            />
-          </button>
-        ) : (
-          <button
-            className="mobile-menu__button mobile-menu__button--sign-in"
-            onClick={() => {
-              onLoginClick();
-              onClose();
-            }}>
-            Sign in
-          </button>
-        )}
+          {isLoggedIn && (
+            <NavLink
+              to="/saved-news"
+              className={`mobile-menu__link ${
+                currentPage === "/saved-news" ? "mobile-menu__link--active" : ""
+              }`}
+              onClick={onClose}>
+              Saved articles
+            </NavLink>
+          )}
+
+          {isLoggedIn ? (
+            <button
+              className={`mobile-menu__button mobile-menu__button--logged-in ${
+                currentPage === "/"
+                  ? "mobile-menu__button--white"
+                  : "mobile-menu__button--black"
+              }`}
+              onClick={() => {
+                onLogout();
+                onClose();
+              }}>
+              <span className="mobile-menu__username">{currentUser.name}</span>
+              <img
+                src={currentPage === "/" ? logOutWhite : logOutBlack}
+                alt="logout"
+                className="mobile-menu__logout-icon"
+              />
+            </button>
+          ) : (
+            <button
+              className="mobile-menu__button mobile-menu__button--sign-in"
+              onClick={() => {
+                onLoginClick();
+                onClose();
+              }}>
+              Sign in
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
