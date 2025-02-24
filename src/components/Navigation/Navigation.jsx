4aa7afde8d21cbd8d68b88a489d1549741fd23c5
kaylieryan <kayliemryan@gmail.com>
2025-02-24 18:15:24 +0000

    Revert "separate mobile menu"
    This reverts commit 120b21527c94ec78e0029d91e92f76389b2aa94c.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,7 +7,6 @@ import { NavLink } from "react-router-dom";
 import { useContext, useState } from "react";
 import { currentPageContext } from "../../contexts/currentPageContext";
 import { currentUserContext } from "../../contexts/currentUserContext";
-import MobileMenu from "../MobileMenu/MobileMenu";
 
 function Navigation({ onLoginClick, onLogout }) {
   const { currentPage } = useContext(currentPageContext);
@@ -23,114 +22,102 @@ function Navigation({ onLoginClick, onLogout }) {
   };
 
   return (
-    <>
-      <nav
-        className={`navigation ${
-          currentPage === "/saved-news" ? "navigation--saved-news" : ""
+    <nav
+      className={`navigation ${
+        currentPage === "/saved-news" ? "navigation--saved-news" : ""
+      }`}>
+      <div className="navigation__container">
+        <NavLink
+          to="/"
+          className="navigation__logo-link"
+          onClick={handleMenuClose}>
+          <img
+            src={currentPage === "/" ? headerWhiteLogo : headerBlackLogo}
+            alt="NewsExplorer Logo"
+            className={`navigation__logo ${
+              currentPage === "/"
+                ? "navigation__logo--white"
+                : "navigation__logo--black"
+            }`}
+          />
+        </NavLink>
+
+        <button
+          className={`navigation__menu-button ${
+            menuOpen ? "navigation__menu-button--open" : ""
+          } ${
+            currentPage === "/saved-news"
+              ? "navigation__menu-button--black"
+              : ""
+          }`}
+          onClick={handleMenuToggle}>
+          <span className="navigation__menu-button-line"></span>
+          <span className="navigation__menu-button-line"></span>
+        </button>
+      </div>
+
+      <div
+        className={`navigation__menu ${
+          menuOpen ? "navigation__menu--open" : ""
         }`}>
-        <div className="navigation__container">
+        <div className="navigation__menu-content">
           <NavLink
             to="/"
-            className="navigation__logo-link"
-            onClick={handleMenuClose}>
-            <img
-              src={currentPage === "/" ? headerWhiteLogo : headerBlackLogo}
-              alt="NewsExplorer Logo"
-              className={`navigation__logo ${
-                currentPage === "/"
-                  ? "navigation__logo--white"
-                  : "navigation__logo--black"
-              }`}
-            />
-          </NavLink>
-
-          <button
-            className={`navigation__menu-button ${
-              menuOpen ? "navigation__menu-button--open" : ""
-            } ${
-              currentPage === "/saved-news"
-                ? "navigation__menu-button--black"
+            className={`navigation__link ${
+              currentPage === "/"
+                ? "navigation__link--active navigation__link--home"
                 : ""
             }`}
-            onClick={handleMenuToggle}>
-            <span className="navigation__menu-button-line"></span>
-            <span className="navigation__menu-button-line"></span>
-          </button>
-        </div>
+            onClick={handleMenuClose}>
+            Home
+          </NavLink>
 
-        <div
-          className={`navigation__menu ${
-            menuOpen ? "navigation__menu--open" : ""
-          }`}>
-          <div className="navigation__menu-content">
+          {isLoggedIn && (
             <NavLink
-              to="/"
+              to="/saved-news"
               className={`navigation__link ${
-                currentPage === "/"
-                  ? "navigation__link--active navigation__link--home"
+                currentPage === "/saved-news"
+                  ? "navigation__link--active navigation__link--saved"
                   : ""
+              } ${
+                currentPage === "/saved-news" ? "navigation__link--black" : ""
               }`}
               onClick={handleMenuClose}>
-              Home
+              Saved articles
             </NavLink>
+          )}
 
-            {isLoggedIn && (
-              <NavLink
-                to="/saved-news"
-                className={`navigation__link ${
-                  currentPage === "/saved-news"
-                    ? "navigation__link--active navigation__link--saved"
-                    : ""
-                } ${
-                  currentPage === "/saved-news" ? "navigation__link--black" : ""
-                }`}
-                onClick={handleMenuClose}>
-                Saved articles
-              </NavLink>
-            )}
-
-            {isLoggedIn ? (
-              <button
-                className={`navigation__button navigation__button--logged-in ${
-                  currentPage === "/"
-                    ? "navigation__button--logged-in-white"
-                    : "navigation__button--logged-in-black"
-                }`}
-                onClick={() => {
-                  onLogout();
-                  handleMenuClose();
-                }}>
-                <span className="navigation__username">{currentUser.name}</span>
-                <img
-                  src={currentPage === "/" ? logOutWhite : logOutBlack}
-                  alt="logout"
-                  className="navigation__logout-icon"
-                />
-              </button>
-            ) : (
-              <button
-                className="navigation__button navigation__button--sign-in"
-                onClick={() => {
-                  onLoginClick();
-                  handleMenuClose();
-                }}>
-                Sign in
-              </button>
-            )}
-          </div>
+          {isLoggedIn ? (
+            <button
+              className={`navigation__button navigation__button--logged-in ${
+                currentPage === "/"
+                  ? "navigation__button--logged-in-white"
+                  : "navigation__button--logged-in-black"
+              }`}
+              onClick={() => {
+                onLogout();
+                handleMenuClose();
+              }}>
+              <span className="navigation__username">{currentUser.name}</span>
+              <img
+                src={currentPage === "/" ? logOutWhite : logOutBlack}
+                alt="logout"
+                className="navigation__logout-icon"
+              />
+            </button>
+          ) : (
+            <button
+              className="navigation__button navigation__button--sign-in"
+              onClick={() => {
+                onLoginClick();
+                handleMenuClose();
+              }}>
+              Sign in
+            </button>
+          )}
         </div>
-      </nav>
-
-      <MobileMenu 
-        isOpen={menuOpen}
-        onClose={handleMenuClose}
-        currentPage={currentPage}
-        isLoggedIn={isLoggedIn}
-        currentUser={currentUser}
-        onLoginClick={onLoginClick}
-        onLogout={onLogout}
-      />
-    </>
+      </div>
+    </nav>
   );
 }
 
